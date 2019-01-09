17beb04038be5bfe047de20ebbcb0e5d85cae108
Lozanito <rodalimj@gmail.com>
2019-01-10 00:41:14 +0100

    Configurado Parametros

--- ControlDinamico/Pure_Pursuit/control_persecucion.m
+++ ControlDinamico/Pure_Pursuit/control_persecucion.m
@@ -12,7 +12,7 @@ function [out]=control_persecucion(in)
     
     kp=2.2;
     kbeta=-0.1;
-    kalf=kp+3.6; 
+    kalf=kbeta+5.8; 
     % Se evita la indeterminacion en omega
     if (t<1e-18) 
         L=0.000001;
